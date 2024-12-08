1a94465efc6d229578549ca4220fe6718df3b3f5
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-12-08 13:13:50 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import BeautifulRoom from "@/components/beautifulRoom/BeautifulRoom";
+import Browserange from "@/components/browserange/Browserange";
+import Hero from "@/components/hero/Hero";
+import OurProducts from "@/components/ourproducts/OurProducts";
+
+
+export default function Home() {
+  return (
+    <>
+    <Hero/>
+    <Browserange/>
+     <OurProducts/>
+    <BeautifulRoom/>
+    </>
+  );
+}
